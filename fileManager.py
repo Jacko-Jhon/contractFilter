99471fd8f0b5b35abc883fc815ca4b78b32d5c57
罗碧文 <120319212+luobiwen@users.noreply.github.com>
2024-03-13 20:23:14 +0800

    Update fileManager.py

--- fileManager.py
+++ fileManager.py
@@ -1,11 +1,8 @@
-#合同分类器
 import os
 import re
 import shutil
-versions = ['0.4', '0.5', '0.6', '0.7', '0.8']
-for version in versions:
-    os.makedirs(os.path.join(r"D:\pythona", version), exist_ok=True)#在此输入你保存分类文件的地址
-for root, dirs, files in os.walk(r"D:\pythona\contracts"):#在此输入你的合同地址
+main_versions = ['0.4', '0.5', '0.6', '0.7', '0.8']
+for root, dirs, files in os.walk(r"D:\pythona\contracts"):#合同地址
     for file in files:
         if file.endswith('.sol'):
             file_path = os.path.join(root, file)
@@ -14,8 +11,11 @@ for root, dirs, files in os.walk(r"D:\pythona\contracts"):#在此输入你的合
                 match = re.search(r"pragma solidity \^(\d+\.\d+\.\d+);", content)
                 if match:
                     version = match.group(1)[:3]
-                    print(version)
-                    if version in versions:
-                        dest_folder = os.path.join(r"D:\pythona", version)
+                    sub_version = match.group(1)
+                    if version in main_versions:
+                        dest_folder = os.path.join(r"D:\pythona", version, sub_version)#分类好需要存放的位置
+                        if not os.path.exists(dest_folder):
+                            os.makedirs(dest_folder)
+                            print("Created target folder", dest_folder)
                         print("Copying", file, "to target folder", dest_folder)
                         shutil.copy(file_path, dest_folder)
